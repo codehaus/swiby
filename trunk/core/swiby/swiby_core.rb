dc26ea129ce2333626b83798f06b6a12875ba158
paul <paul@6b69f6a9-7b3a-0410-99a9-cb42ce969041>
2007-10-07 19:07:43 +0000

    Igor's rspec tests

--- trunk/core/swiby/swiby_core.rb
+++ trunk/core/swiby/swiby_core.rb
@@ -34,210 +34,133 @@ module Swiby
 
   include_class 'javax.swing.JScrollPane'
     
-	class SwingBase
-		
-		def self.swing_attr_accessor(symbol, *args)
-			
-			map, err = to_map(symbol, args)
-			
-			raise RuntimeError, err if not err.nil?
+  class SwingBase
 
-			generate_swing_attribute map, true
-			
-		end
-		
-		def self.swing_attr_writer(symbol, *args)
-			
-			map, err = to_map(symbol, args)
-			
-			raise RuntimeError, err if not err.nil?
+    def self.swing_attr_accessor(symbol, *args)
+      generate_swing_attribute to_map(symbol, args), :include_attr_reader
+    end
 
-			generate_swing_attribute map, false
-			
-		end
-		
-		def self.container(symbol)
-		
-			raise RuntimeError, "#{symbol} is not a Symbol" if not symbol.is_a? Symbol
-			
-			eval %{
-				
-				def #{symbol}(array = nil, &block)
-				
-					if array.nil?
-						self.addComponents block.call
-					else
-						self.addComponents array
-					end
-					
-				end
-			
-			}
-			
-		end
-	
-		def scrollable
-			@scroll_pane = JScrollPane.new @component
-		end
-	
-		def install_listener iv
-		end
-		
-		def java_component
-			
-			if @scroll_pane.nil?
-				@component
-			else
-				@scroll_pane
-			end
-			
-		end
-		
-		#TODO remove private because some calls to 'addComponents' were forbidden (with JRuby 1.0.1)
-		#private
-		
-		def self.to_map(symbol, args)
-		
-			map = Hash.new
-			
-			err = fill_map map, symbol
-			
-			return nil, err if not err.nil?
-			
-			return map, nil if args.nil?
-			
-			args.each do |sym|
-			
-				err = fill_map map, sym
-				
-				return nil, err if not err.nil?
-				
-			end
-			
-			return map, nil
-			
-		end
-		
-		def self.fill_map(map, x)
-		
-			if x.is_a? Hash
-			
-				x.each do |k, v|
-				
-					return "#{k} must be a Symbol" if not k.is_a? Symbol
-				
-					map[k] = v
-					
-				end
-				
-			elsif x.is_a? Symbol
-				map[x] = nil
-			else
-				return "#{x} must be a Symbol"
-			end
-			
-			nil
-			
-		end
-		
-		def self.generate_swing_attribute(map, both)
-			
-			map.each do |symbol, javaName|
-			
-				javaName = symbol if javaName.nil?
-			
-				if both
-				
-					class_eval %{
-						
-						def #{symbol}
-							@component.#{javaName}
-						end
-						
-					}
+    def self.swing_attr_writer(symbol, *args)
+      generate_swing_attribute to_map(symbol, args)
+    end
 
-				end
-				
-				class_eval %{
-					
-					def #{symbol}=(val)
-						@component.#{javaName} = val
-					end
-					
-					def #{symbol}(val = nil, &block)
-						
-						if val.instance_of? IncrementalValue
-						
-							val.assign_to self, :#{symbol}=
-							
-							install_listener val
-							
-						else
-						
-							if val.nil?
-								val = block.call if not block.nil?
-							end
-				
-							self.#{symbol} = val
-							
-						end
-						
-					end
-					
-				}
-				
-			end
-	
-		end
-		
-		def addComponents(array)
-		
-			if array.respond_to? :each
-		
-				array.each do |comp|
-					 @component.add comp.java_component
-				end
-				
-			else
-				@component.add array.java_component
-			end
-			
-		end
-		
-	end
-		
-	def self.component_factory(method)
-	
-		class_name = ""
-		
-		code = Proc.new do 
-			%{
-				
-				def #{method}(&block)
-					x = ::#{class_name}.new
-					x.instance_eval(&block)
-					x
-				end
-				
-			}
-		end
-	
-		if method.is_a? Symbol or method.is_a? String
-			
-			class_name = method
-			
-			eval code.call
-			
-		elsif method.is_a? Hash
-			
-			method.each do |method, class_name|
-				eval code.call
-			end
-		
-		else
-			raise RuntimeError, "#{method} should be a Symbol or a Hash"
-		end
-		
-	end
+    def self.container(symbol)
+      raise RuntimeError, "#{symbol} is not a Symbol" unless symbol.is_a? Symbol
 
+      eval %{
+        def #{symbol}(array = nil, &block)
+          if array.nil?
+            self.addComponents block.call
+          else
+            self.addComponents array
+          end
+        end
+      }
+    end
+
+    def scrollable
+      @scroll_pane = JScrollPane.new @component
+    end
+
+    def install_listener iv
+    end
+
+    def java_component
+      return @component if @scroll_pane.nil?
+      @scroll_pane
+    end
+
+    #TODO remove private because some calls to 'addComponents' were forbidden (with JRuby 1.0.1)
+    #private
+
+    def self.to_map(symbol, args)
+      map = Hash.new
+      fill_map map, symbol
+      args.each {|sym| fill_map map, sym } unless args.nil?
+      map
+    end
+
+    def self.fill_map(map, x)
+      if x.is_a? Hash
+        x.each do |k, v|
+          raise RuntimeError, "#{k} must be a Symbol" unless k.is_a? Symbol
+          map[k] = v
+        end
+      elsif x.is_a? Symbol
+        map[x] = nil
+      else
+        raise RuntimeError, "#{x} must be a Symbol"
+      end
+      map
+    end
+
+    def self.generate_swing_attribute(map, include_attr_reader=nil)
+      map.each do |symbol, javaName|
+        javaName ||= symbol
+        if include_attr_reader
+          class_eval %{
+            def #{symbol}
+              @component.#{javaName}
+            end
+          }
+        end
+      
+        class_eval %{
+
+          def #{symbol}=(val)
+            @component.#{javaName} = val
+          end
+
+          def #{symbol}(val = nil, &block)
+            if val.instance_of? IncrementalValue
+              val.assign_to self, :#{symbol}=
+              install_listener val
+            else
+              if val.nil?
+                val = block.call if not block.nil?
+              end
+              self.#{symbol} = val
+            end
+           end
+
+        }
+      end
+
+    end
+
+    def addComponents(array)
+      if array.respond_to? :each
+        array.each do |comp|
+          @component.add comp.java_component
+        end
+      else
+        @component.add array.java_component
+      end
+    end
+  end
+
+  def self.component_factory(method)
+    class_name = ""
+    code = Proc.new do 
+      %{
+	def #{method}(&block)
+	  x = ::#{class_name}.new
+	  x.instance_eval(&block)
+	  x
+    	end
+      }
+    end
+
+    if method.is_a? Symbol or method.is_a? String
+      class_name = method
+      eval code.call
+    elsif method.is_a? Hash
+      method.each do |method, class_name|
+        eval code.call
+      end
+    else
+      raise RuntimeError, "#{method} should be a Symbol or a Hash"
+    end
+  end
+  
 end
