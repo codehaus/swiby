7184364ae3716c05260fd1b8d384a3f02ea18e50
jeanlazarou <jeanlazarou@6b69f6a9-7b3a-0410-99a9-cb42ce969041>
2008-10-05 14:30:17 +0000

    Add password field support

---
+++ core/test/manual/password_test.rb
@@ -0,0 +1,39 @@
+#--
+# Copyright (C) Swiby Committers. All rights reserved.
+# 
+# The software in this package is published under the terms of the BSD
+# style license a copy of which has been included with this distribution in
+# the LICENSE.txt file.
+#
+#++
+
+class PasswordTest < ManualTest
+
+  manual 'Password input' do
+
+    form {
+      
+      title 'Login...'
+
+      width 200
+      height 160
+      
+      content {
+        section 'Click OK to check input'
+          input "Login:", "", :name => :login
+          password "Password:", "", :name => :pwd
+        
+          button('Ok') {
+            message_box "Login is #{context[:login].value}/#{context[:pwd].value}"
+          }
+         
+      }
+      
+      visible true
+      
+    }
+
+  end
+  
+  
+end
