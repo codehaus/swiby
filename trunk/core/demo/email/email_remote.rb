00ad5e4c815b120a1d984fd618f436499a40fbaa
jeanlazarou <jeanlazarou@6b69f6a9-7b3a-0410-99a9-cb42ce969041>
2009-04-05 14:42:37 +0000

    changed email demo to be 'SWEB', it downloads the ruby/swiby application from a remote server, here the Pico Web Remoting (PWR) server implementing the email demo. See http://picocontainer.org/web/remoting.html the application files served by the PWR server are in the demo/email/deploy folder

--- trunk/core/demo/email/email_remote.rb
+++ trunk/core/demo/email/email_remote.rb
@@ -11,299 +11,63 @@
 # Run this script with '-json' switch to use JSON exhcange format,
 # otherwise it uses Ruby format
 #
+# see comment about required gems in pwr_remoting
+#
 
-# To use JSON format, need to install some gem:
-#         gem install json-jruby
-#         or jruby -S gem install json-jruby
+#
+# This script is just testing the Email PWR demo, it outputs results
+# to the console
+#
 
-#either use
-#     require 'rubygems'
-# either set env variable
-#     set JRUBY_OPTS=-rubygems
-require 'rubygems'
-require 'erb'
-require 'net/http'
+require 'pwr_remoting'
 
-class JSONParser
-  
-  attr_accessor :last_error
-  
-  def parse data
-    symbolize!(JSON.parse(data))
-  end
-  
-  def parse? data
-    
-    @last_error = nil
-    
-    @last_error = symbolize!(JSON.parse(data)) unless data.chomp == "\"OK\""
-    
-    @last_error.nil?
-    
-  end
-  
-  def last_error
-    @last_error
-  end
-  
-  private
-  
-  # change keys names in hash maps (JSON objects) from String type to Ruby symbols (more rubish)
-  def symbolize! data
-    
-    data.each {|el| symbolize!(el)} if data.is_a?(Array)
-    
-    if data.is_a?(Hash)
-      
-      h = data.to_a
-      data.clear
-      
-      h.each do |pair|
-        data[pair[0].to_sym] = symbolize!(pair[1])
-      end
-    
-    end
-  
-    data
-    
-  end
-  
+options = parse_remoting_options(ARGV)
+
+if options.use_json
+  puts 'Using JSON format'
+  require 'json'
+else
+  puts 'Using Ruby format'
 end
 
-class RubyParser
-  
-  attr_accessor :last_error
-  
-  def parse data
-    eval(data)
-  end
-  
-  def parse? data
-    
-    @last_error = nil
-    
-    data = eval(data)
-    
-    @last_error = data unless data == "OK"
-    
-    @last_error.nil?
-    
-  end
-  
-  def last_error
-    @last_error
-  end
-  
+auth = Auth.new(create_connection(options))
+
+unless auth.login('Gil Bates', '1234')
+#unless auth.login('Beeve Salmer', '1234')
+  puts auth.last_error[:message]
+  exit
 end
 
-class Connection
-  
-  def initialize server, port, service_path, parser
-    @http = Net::HTTP.new(server, port)
-    @service_path = service_path
-    @headers = nil
-    @parser = parser
-  end
+puts '::inbox'
+p auth.inbox.messages
+puts '::sent'
+p auth.sentbox.messages
 
-  def submit remote_class, method, args = {}
-    @parser.parse(post(remote_class, method, args))
-  end
-  
-  def submit? remote_class, method, args = {}
-    @parser.parse?(post(remote_class, method, args))
-  end
-  
-  def last_error
-    @parser.last_error
-  end
-  
-  def last_error= last_error
-    @parser.last_error = last_error
-  end
-  
-  private
-  
-  def post remote_class, method, args
-    
-    path = "#{@service_path}/#{remote_class}/#{method}"
-    
-    body = args.map {|k,v| "#{k}=#{ERB::Util.url_encode(v)}"}.join("&")
-    
-    resp, data = @http.start { |http| http.post(path, body, @headers) }
-    
-    unless resp.kind_of?(Net::HTTPSuccess)
-      raise "URI '#{path}' not found" if resp.is_a?(Net::HTTPNotFound)
-      raise "Request error '#{resp.message}' for '#{path}'"
-    end
-    
-    @headers = { 'Cookie' => resp.response['set-cookie']} if resp.response['set-cookie']
-    
-    data
-    
-  end
-  
+puts '::read message 1'
+p auth.inbox.read 1
+
+puts '::read message 1999'
+unless auth.inbox.read 1999
+  puts auth.inbox.last_error[:message]
 end
-
-class Auth
-  
-  def initialize connection
-    @connection = connection
-  end
-  
-  def login user, password
-    @connection.submit?(self.class, 'logIn', :userName => user, :password => password)
-  end
-  
-  def logout
-    @connection.submit?(self.class, 'logOut')    
-  end
-  
-  def inbox
-    @inbox = Inbox.new(@connection) unless @inbox
-    @inbox
-  end
-  
-  def sentbox
-    @sentbox = Sent.new(@connection) unless @sentbox
-    @sentbox
-  end
-  
-  def last_error
-    @connection.last_error
-  end
-  
-end
-
-class Mailbox
-  
-  def initialize connection
-    @connection = connection
-  end
-  
-  def messages
-    @connection.submit(self.class, 'messages')
-  end
-  
-  def delete id
-    @connection.submit?(self.class, 'delete', :msgId => id)
-  end
-  
-  def read id
-    
-    message = @connection.submit(self.class, 'read', :msgId => id)
-    
-    @connection.last_error = message if message[:ERROR]
-    
-    message = nil if @connection.last_error
-    
-    message
-    
-  end
-  
-  def last_error
-    @connection.last_error
-  end
-  
-end
-
-class Inbox < Mailbox
-end
-
-class Sent < Mailbox
-  
-  def send subject, message, to
-    @connection.submit?(self.class, 'send', :subject => subject, :message => message, :to => to)
-  end
-  
-end
-
-def parse_options args, app_name = __FILE__, version = '1.0'
-
-  require 'swiby/util/arguments_parser'
-
-  parser = create_parser_for(app_name, version) {
-    
-    accept optional, :port, '-p', '--port port_number', 'Server port (defaults to 80)'
-    accept required, :host, '-h', '--host host_name', 'Server host name'
-    accept required, :service, '-s', '--service service_path', 'Path for email service'
-    accept optional, :use_json, '-j', '--json', :doc => "Use JSON instead of Ruby data transfer\n(default to Ruby)"
-    
-    validator do |options|
-      options.port = 80 unless options.port
-    end
-    
-  }
-  
-  options = parser.parse(args)
-
-  require 'json' if options.use_json
-
-  options
-  
-end
-
-def create_connection options
-  
-  parser = options.use_json ? JSONParser.new : RubyParser.new
-  
-  puts "Connection to #{options.host}:#{options.port}#{options.service}"
-  puts "Using JSON data format" if options.use_json
-  
-  Connection.new(options.host, options.port, options.service, parser)
-  
-end
-
-if $0 == __FILE__
-
-  options = parse_options(ARGV)
-  
-  if options.use_json
-    puts 'Using JSON format'
-    require 'json'
-  else
-    puts 'Using Ruby format'
-  end
-
-  auth = Auth.new(create_connection(options))
-  
-  unless auth.login('Gil Bates', '1234')
-  #unless auth.login('Beeve Salmer', '1234')
-    puts auth.last_error[:message]
-    exit
-  end
-
-  puts '::inbox'
-  p auth.inbox.messages
-  puts '::sent'
-  p auth.sentbox.messages
-
-  puts '::read message 1'
-  p auth.inbox.read 1
-
-  puts '::read message 1999'
-  unless auth.inbox.read 1999
-    puts auth.inbox.last_error[:message]
-  end
 
 =begin
-  puts '::delete 1'
-  unless auth.inbox.delete 1
-    puts auth.inbox.last_error[:message]
-  end
+puts '::delete 1'
+unless auth.inbox.delete 1
+  puts auth.inbox.last_error[:message]
+end
 
-  puts '::inbox'
-  p auth.inbox.messages
+puts '::inbox'
+p auth.inbox.messages
 =end
 
-  puts '::delete 7777'
-  unless auth.inbox.delete 7777
-    puts auth.inbox.last_error[:message]
-  end
-
-  puts '::send mail + show sent box'
-  auth.sentbox.send 'news', 'Misual Vasic .ORG', 'James Bond'
-  p auth.sentbox.messages
-
-  auth.logout
-
+puts '::delete 7777'
+unless auth.inbox.delete 7777
+  puts auth.inbox.last_error[:message]
 end
+
+puts '::send mail + show sent box'
+auth.sentbox.send 'news', 'Misual Vasic .ORG', 'James Bond'
+p auth.sentbox.messages
+
+auth.logout
