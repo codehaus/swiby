f4e0f8a08388733988af2abb33b1d9f2cc8123c6
jeanlazarou <jeanlazarou@6b69f6a9-7b3a-0410-99a9-cb42ce969041>
2009-04-18 08:46:28 +0000

    forgot to add license header

--- core/demo/email/deploy/login_controller_test.rb
+++ core/demo/email/deploy/login_controller_test.rb
@@ -1,3 +1,11 @@
+#--
+# Copyright (C) Swiby Committers. All rights reserved.
+# 
+# The software in this package is published under the terms of the BSD
+# style license a copy of which has been included with this distribution in
+# the LICENSE.txt file.
+#
+#++
 
 require 'test/unit'
 require 'swiby/mvc/test'
