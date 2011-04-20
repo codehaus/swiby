b61bc901f3295b4139fc1dee1aaf911a17ec6bff
jeanlazarou <jeanlazarou@6b69f6a9-7b3a-0410-99a9-cb42ce969041>
2011-04-20 08:57:35 +0000

    simple code layout change

--- core/lib/swiby/component/table.rb
+++ core/lib/swiby/component/table.rb
@@ -212,7 +212,7 @@ module Swiby
               
           end
 
-          addColumn '' if getColumnCount() == 0 #TODO what is better for default table header?
+		  addColumn '' if getColumnCount() == 0 #TODO what is better for default table header?
           addRow vector
 
         end
